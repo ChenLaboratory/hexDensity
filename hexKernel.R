78e666be7a5527959f5dfdfa7859acfe5c8a5988
HoangEevee <nguyenquochoang2806@gmail.com>
2023-10-21 23:40:10 +1100

    better kernel calculation

--- hexKernel.R
+++ hexKernel.R
@@ -3,11 +3,11 @@ library(spatstat.explore)
 source("hexbinFullRegular.R")
 
 hexKernel = function(x,y=NULL, 
-                     xbins = 128, 
+                     xbins = 128, #128 is the magic number in pixellate of spatstat
                      sigma = 1,
                      edge = TRUE,
                      diggle = FALSE) {
-  hbin = hexbinFullRegular(x,y=NULL,xbins=xbins) #128 is the magic number in pixellate of spatstat
+  hbin = hexbinFullRegular(x,y=NULL,xbins=xbins) 
   row = hbin@dimen[1]
   col = hbin@dimen[2]
   # print(row)
@@ -21,19 +21,33 @@ hexKernel = function(x,y=NULL,
   }
   
   #Make kernel
-  kernel = matrix(0,nrow = 2*row, ncol = 2*col+row-1)
-  center.r = row
-  center.q = col+row/2
-  for (i in seq(1, row)) {
-    for (j in seq(1,2*col)) {
-      q = j+row-i
-      kernel[i*2,q] = dnorm(hexSize*euclidDistance(center.q,center.r,q,2*i),sd=sigma) * dnorm(0,sd=sigma)
-      kernel[i*2-1,q] = dnorm(hexSize*euclidDistance(center.q,center.r,q,2*i-1),sd=sigma) * dnorm(0,sd=sigma)
-    }
-  } 
-  #print(sum(kernel))
-  kernel = kernel/sum(kernel)
   
+  # Older,slower kernel. Keeping just in case
+  # kernel = matrix(0,nrow = 2*row, ncol = 2*col+row-1)
+  # center.r = row
+  # center.q = col+row/2
+  # for (i in seq(1, row)) {
+  #   for (j in seq(1,2*col)) {
+  #     q = j+row-i
+  #     kernel[i*2,q] = dnorm(hexSize*euclidDistance(center.q,center.r,q,2*i),sd=sigma) * dnorm(0,sd=sigma)
+  #     kernel[i*2-1,q] = dnorm(hexSize*euclidDistance(center.q,center.r,q,2*i-1),sd=sigma) * dnorm(0,sd=sigma)
+  #   }
+  # }
+
+  kernel.left.hori = dnorm(hexSize*c(seq(col,1),seq(0,col-1)),sd=sigma)
+  kernel.left.verti = dnorm(hexSize*sqrt(3)*c(seq(row/2-1,0),seq(1,row/2)),sd=sigma)
+  kernel.left=outer(kernel.left.verti,kernel.left.hori)
+
+  kernel.right.hori = dnorm(hexSize*(c(seq(col,1)-0.5,seq(0,col-1)+0.5)),sd=sigma)
+  kernel.right.verti = dnorm(hexSize*sqrt(3)*c(seq(row/2-1,0)+0.5,seq(1,row/2)-0.5),sd=sigma)
+  kernel.right=outer(kernel.right.verti,kernel.right.hori)
+
+  kernel = matrix(0,nrow = 2*row, ncol = 2*col+row-1)
+  for (i in seq(1, row)) {
+    kernel[i*2-1,(1+row-i):(1+row-i+2*col-1)] = rev(kernel.right[i,])
+    kernel[i*2,(1+row-i):(1+row-i+2*col-1)] = rev(kernel.left[i,])
+  }
+
   #inverse the kernel
   kernel.inv = matrix(0,nrow = 2*row, ncol = 2*col+row-1)
   #going clock-wise from top-left of inverse kernel
@@ -52,10 +66,8 @@ hexKernel = function(x,y=NULL,
     }
     fM = fft2D(mask)
     con = fft2D(fM * fK, inverse=TRUE)
-    con = con/(2*row*(2*col+row-1))
+    con = con/(2*row*(2*col+(row-1)))
     edg <- Mod(con[1:row, 1:(col+(row-1)/2)])
-    # return(edg)
-    # print(edg)
     if(diggle) {
       staggeredBin[1:row,1:(col+(row-1)/2)] = staggeredBin[1:row,1:(col+(row-1)/2)]/edg 
     }
@@ -64,8 +76,8 @@ hexKernel = function(x,y=NULL,
 
   #KDE calculation
   fY = fft2D(staggeredBin)
-  sm = fft2D(fY*fK,inverse = TRUE)/(2*row*(2*col+row-1))
-  
+  sm = fft2D(fY*fK,inverse = TRUE)/(2*row*(2*col+(row-1)))
+
   #extract back to hexbin class
   count = c()
   if(edge & !diggle){
@@ -81,7 +93,6 @@ hexKernel = function(x,y=NULL,
   }
 
   hbin@count = count
-  #print("done")
   return(hbin)
   
 }
@@ -98,6 +109,5 @@ euclidDistance = function(q1,r1,q2,r2) {
   
   x2 = sqrt(3)*q2 +(sqrt(3)/2) *r2
   y2 = (3/2)*r2
-  
-  return (sqrt((x1-x2)**2 + (y1-y2)**2))
+  return (sqrt(((x1-x2)**2 + (y1-y2)**2)/3))
 }
