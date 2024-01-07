cf441fc93a8b0bb5bff59ece1e4d0f8a51f08a18
HoangEevee <nguyenquochoang2806@gmail.com>
2024-01-07 15:17:25 +1100

    weeding unused arguments from hexbin plot

--- R/plotHexDensity.R
+++ R/plotHexDensity.R
@@ -28,26 +28,19 @@ plotHexDensity = function(hexDensity,
 }
 
 gplot.hexbin <-
-  function(x, style = "colorscale",
+  function(x,
            legend = 1.2, lcex = 1,
-           minarea = 0.04, maxarea = 0.8, mincnt = 1, maxcnt = max(x@count),
+          mincnt = 1, maxcnt = max(x@count),
            trans = NULL, inv = NULL,
            colorcut = seq(0, 1, length = min(17, maxcnt)),
-           border = NULL, density = NULL, pen = NULL,
            colramp = function(n) LinGray(n, beg = 90, end = 15),
            xlab = NULL, ylab = NULL, main = "", newpage = TRUE,
-           type = c("p", "l", "n"), xaxt = c("s", "n"), yaxt = c("s", "n"),
+           type = TRUE, xaxt = TRUE, yaxt = TRUE,
            clip="on", verbose = getOption("verbose"),
            test)
   {
   if(!is(x,"hexbin"))
     stop("first argument must be a hexbin object")
-  if(minarea < 0)
-    stop("Minimum area must be non-negative")
-  if(maxarea > 1)
-    warning("Maximum area should be <= 1 this leads to overlapping hexagons")
-  if(minarea > maxarea)
-    stop("Minarea must be <= maxarea")
   if (length(colorcut) > 1) { # a sequence 0,...,1
     if(colorcut[1] != 0)
       stop("Colorcut lower boundary must be 0")
@@ -60,18 +53,14 @@ gplot.hexbin <-
     else 1
   }
   
-  type <- match.arg(type)
-  xaxt <- match.arg(xaxt)
-  yaxt <- match.arg(yaxt)
-  
   ## ----- plotting starts ------------------------
   if (newpage) grid::grid.newpage()
   hv.ob <- hexbin::hexViewport(x, xbnds=x@xbnds, ybnds=x@ybnds,
                        offset = grid::unit(legend,"inches"))
   grid::pushViewport(hv.ob@hexVp.off)
   
-  if(xaxt != "n") grid.xaxis()
-  if(yaxt != "n") grid.yaxis()
+  if(xaxt) grid.xaxis()
+  if(yaxt) grid.yaxis()
   ## xlab, ylab, main :
   if(is.null(xlab)) xlab <- x@xlab
   if(is.null(ylab)) ylab <- x@ylab
@@ -82,15 +71,14 @@ gplot.hexbin <-
   if(nchar(main) > 0)
     grid.text(main, y = grid::unit(1, "npc") + grid::unit(1.5, "lines"),
               gp = gpar(fontsize = 18))
-  if(type != "n") {
+  if(type) {
     if(clip == "on") {
       grid::upViewport()
       grid::pushViewport(hv.ob@hexVp.on)
     }
-    grid.hexagons(x, style = style, minarea = minarea, maxarea = maxarea,
+    grid.hexagons(x,
                   mincnt = mincnt, maxcnt = maxcnt, check.erosion = FALSE,
-                  trans = trans, colorcut = colorcut, density = density,
-                  border = border, pen = pen,
+                  trans = trans, colorcut = colorcut,
                   colramp = colramp, verbose = verbose,
                   test=test)
   }
@@ -115,7 +103,7 @@ hexcoords <- function(dx, dy = NULL, n = 1, sep = NULL)
 
 hexpolygon <-
   function(x, y, hexC = hexcoords(dx, dy, n = 1), dx, dy=NULL,
-           fill = 1, border = 0, hUnit = "native", ...)
+           fill = 1, hUnit = "native", ...)
   {
     ## Purpose: draw hexagon [grid.]polygon()'s  around  (x[i], y[i])_i
     ## Author: Martin Maechler, Jul 2004; Nicholas for grid
@@ -129,13 +117,13 @@ hexpolygon <-
         grid.polygon(x = grid::unit(rep.int(hexC$x, n) + rep.int(x, n6),hUnit),
                      y = grid::unit(rep.int(hexC$y, n) + rep.int(y, n6),hUnit),
                      id.lengths = n6,
-                     gp = gpar(col= border, fill= fill))
+                     gp = gpar(col=F,fill= fill))
       }
       else {
         grid.polygon(x = rep.int(hexC$x, n) + rep.int(x, n6),
                      y = rep.int(hexC$y, n) + rep.int(y, n6),
                      id.lengths = n6,
-                     gp = gpar(col= border, fill= fill))
+                     gp = gpar(col=F,fill= fill))
       }
     }
     else{ ## traditional graphics polygons: must be closed explicitly (+ 1 pt)
@@ -146,13 +134,11 @@ hexpolygon <-
   }
 
 grid.hexagons <-
-  function(dat, style = c("colorscale", "centroids", "lattice",
-                          "nested.lattice", "nested.centroids", "constant.col"),
+  function(dat,
            use.count=TRUE, cell.at=NULL,
-           minarea = 0.05, maxarea = 0.8, check.erosion = TRUE,
+           check.erosion = TRUE,
            mincnt = 1, maxcnt = max(dat@count), trans = NULL,
            colorcut = seq(0, 1, length = 17),
-           density = NULL, border = NULL, pen = NULL,
            colramp = function(n){ LinGray(n,beg = 90, end = 15) },
            def.unit = "native",
            verbose = getOption("verbose"),
@@ -240,11 +226,6 @@ grid.hexagons <-
     ##____________________Initial checks_______________________
     if(!is(dat,"hexbin"))
       stop("first argument must be a hexbin object")
-    style <- match.arg(style) # so user can abbreviate
-    if(minarea <= 0)
-      stop("hexagons cannot have a zero area, change minarea")
-    if(maxarea > 1)
-      warning("maxarea > 1, hexagons may overplot")
     ##_______________ Collect computing constants______________
     
     if(use.count){
@@ -268,95 +249,46 @@ grid.hexagons <-
     sy <- (xbins * shape)/diff(dat@ybnds)
     
     ##___________Transform Counts to Radius_____________________
-    switch(style,
-           "centroids" = ,
-           "lattice" = ,
-           "constant.col" =,
-           "colorscale" = {
-             if(is.null(trans)) {
-               if( min(cnt,na.rm=TRUE)< 0){
-                 pcnt<- cnt + min(cnt)
-                 rcnt <- {
-                   if(maxcnt == mincnt) rep.int(1, length(cnt))
-                   else (pcnt - mincnt)/(maxcnt - mincnt)
-                 }
-               }
-               else rcnt <- {
-                 if(maxcnt == mincnt) rep.int(1, length(cnt))
-                 else (cnt - mincnt)/(maxcnt - mincnt)
-               }
-             }
-             else {
-               rcnt <- (trans(cnt) - trans(mincnt)) /
-                 (trans(maxcnt) - trans(mincnt))
-               if(any(is.na(rcnt)))
-                 stop("bad count transformation")
-             }
-             area <- minarea + rcnt * (maxarea - minarea)
-           },
-           "nested.lattice" = ,
-           "nested.centroids" = {
-             diffarea <- maxarea - minarea
-             step <- 10^floor(log10(cnt))
-             f <- (cnt/step - 1)/9
-             area <- minarea + f * diffarea
-             area <- pmax(area, minarea)
-           }
-    )
-    area <- pmin(area, maxarea)
-    radius <- sqrt(area)
-    
+   if(is.null(trans)) {
+     if( min(cnt,na.rm=TRUE)< 0){
+       pcnt<- cnt + min(cnt)
+       rcnt <- {
+         if(maxcnt == mincnt) rep.int(1, length(cnt))
+         else (pcnt - mincnt)/(maxcnt - mincnt)
+       }
+     }
+     else rcnt <- {
+       if(maxcnt == mincnt) rep.int(1, length(cnt))
+       else (cnt - mincnt)/(maxcnt - mincnt)
+     }
+   }
+   else {
+     rcnt <- (trans(cnt) - trans(mincnt)) /
+       (trans(maxcnt) - trans(mincnt))
+     if(any(is.na(rcnt)))
+       stop("bad count transformation")
+   }
+   # area <- minarea + rcnt * (maxarea - minarea)
+   # 
+   #  area <- pmin(area, maxarea)
+   #  radius <- sqrt(area)
+   #  
     ##______________Set Colors_____________________________
-    switch(style,
-           "centroids" = ,
-           "constant.col" = ,
-           "lattice" = {
-             if(length(pen)!= length(cnt)){
-               if(is.null(pen)) pen <- rep.int(1, length(cnt))
-               ##else if(length(pen)== length(cnt)) break
-               else if(length(pen)== 1) pen <- rep.int(pen,length(cnt))
-               else stop("'pen' has wrong length")
-             }
-           },
-           "nested.lattice" = ,
-           "nested.centroids" = {
-             if(!is.null(pen) && length(dim(pen)) == 2) {
-               dp <- dim(pen)
-               lgMcnt <- ceiling(log10(max(cnt)))
-               if(dp[1] != length(cnt) && dp[1] != lgMcnt ) {
-                 stop ("pen is not of right dimension")
-               }
-               if( dp[1] == lgMcnt ) {
-                 ind <- ceiling(log10(dat@count)) ## DS: 'dat' was 'bin' (??)
-                 ind[ind == 0] <- 1
-                 pen <- pen[ind,]
-               }
-               ##else break
-             }
-             else {
-               pen <- floor(log10(cnt)) + 2
-               pen <- cbind(pen, pen+10)
-             }
-           },
-           "colorscale" = {
              ## MM: Following is quite different from bin2d's
-             nc <- length(colorcut)
-             if(colorcut[1] > colorcut[nc]){
-               colorcut[1] <- colorcut[1] + 1e-06
-               colorcut[nc] <- colorcut[nc] - 1e-06
-             } else {
-               colorcut[1] <- colorcut[1] - 1e-06
-               colorcut[nc] <- colorcut[nc] + 1e-06
-             }
-             colgrp <- cut(rcnt, colorcut,labels = FALSE)
-             if(any(is.na(colgrp))) colgrp <- ifelse(is.na(colgrp),0,colgrp)
-             ##NL: colramp must be a function accepting an integer n
-             ##    and returning n colors
-             clrs <- colramp(length(colorcut) - 1)
-             pen <- clrs[colgrp]
-           }
-    )
-    
+   nc <- length(colorcut)
+   if(colorcut[1] > colorcut[nc]){
+     colorcut[1] <- colorcut[1] + 1e-06
+     colorcut[nc] <- colorcut[nc] - 1e-06
+   } else {
+     colorcut[1] <- colorcut[1] - 1e-06
+     colorcut[nc] <- colorcut[nc] + 1e-06
+   }
+   colgrp <- cut(rcnt, colorcut,labels = FALSE)
+   if(any(is.na(colgrp))) colgrp <- ifelse(is.na(colgrp),0,colgrp)
+   ##NL: colramp must be a function accepting an integer n
+   ##    and returning n colors
+   clrs <- colramp(length(colorcut) - 1)
+   pen <- clrs[colgrp]
     ##__________________ Construct a hexagon___________________
     ## The inner and outer radius for hexagon in the scaled plot
     inner <- 0.5
@@ -367,102 +299,18 @@ grid.hexagons <-
     rad <- sqrt(dx^2 + dy^2)
     hexC <- hexcoords(dx, dy, sep=NULL)
     ##_______________ Full Cell	 Plotting_____________________
-    switch(style,
-           "constant.col" = ,
-           "colorscale" = {
-             if (test) {
-               print("hi")
-               mostFreqPen = names(which.max(table(pen)))
-               grid::grid.rect(gp=gpar(fill=mostFreqPen))
-               notMostFreq=(pen!=mostFreqPen)
-               pen = pen[notMostFreq]
-               xnew = xnew[notMostFreq]
-               ynew = ynew[notMostFreq]
-               print(paste(length(xnew),"hexagons drawn out of",length(dat@count),"hexagons"))
-             }
-             hexpolygon(xnew, ynew, hexC,
-                        density = density, fill = pen,
-                        border = if(!is.null(border)) border else pen)
-             ## and that's been all for these styles
-             return(invisible(paste("done", sQuote(style))))
-           },
-           "nested.lattice" = ,
-           "nested.centroids" = {
-             hexpolygon(xnew, ynew, hexC,
-                        density = density,
-                        fill = if (is.null(border) || border) 1 else pen[,1],
-                        border = pen[,1])
-           }
-    )
-    
-    ##__________________ Symbol Center adjustments_______________
-    if(style == "centroids" || style == "nested.centroids") {
-      xcm <- dat@xcm[good]
-      ycm <- dat@ycm[good]
-      ## Store 12 angles around a circle and the replicate the first
-      ## The actual length for these vectors is determined by using
-      ## factor use below
-      k <- sqrt(3)/2
-      cosx <- c(1, k, .5, 0, -.5, -k, -1, -k,	 -.5, 0, .5,   k, 1)/sx
-      siny <- c(0, .5, k, 1,	 k, .5,	 0, -.5, -k, -1, -k, -.5, 0)/sy
-      ## Compute distances for differences after scaling into
-      ## [0,size] x [0,aspect*size]
-      ## Then there are size hexagons on the x axis
-      dx <- sx * (xcm - xnew)
-      dy <- sy * (ycm - ynew)
-      dlen <- sqrt(dx^2 + dy^2)
-      ## Find the closest approximating direction of the 12 vectors above
-      cost <- ifelse(dlen > 0, dx/dlen, 0)
-      tk <- (6 * acos(cost))/pi
-      tk <- round(ifelse(dy < 0, 12 - tk, tk)) + 1
-      ## Select the available length for the approximating vector
-      hrad <- ifelse(tk %% 2 == 1, inner, outer)
-      ## Rad is either an inner or outer approximating radius.
-      ## If dlen + hrad*radius <= hrad, move the center dlen units.
-      ## Else move as much of dlen as possible without overplotting.
-      fr <- pmin(hrad * (1 - radius), dlen) # Compute the symbol centers
-      ## fr is the distance for the plot [0,xbins] x [0,aspect*xbins]
-      
-      ## cosx and siny give the x and y components of this distance
-      ## in data units
-      xnew <- xnew + fr * cosx[tk]
-      ynew <- ynew + fr * siny[tk]
-    }
-    ## ________________Sized Hexagon Plotting__________________
-    ## scale the symbol by radius and add to the new center
-    n <- length(radius)
-    if(verbose)
-      cat('length = ',length(pen),"\n", 'pen = ', pen+1,"\n")
-    ##switch(style,
-    ##	    centroids = ,
-    ##	    lattice = {if(is.null(pen))pen <- rep.int(1, n)
-    ##			else pen <- rep.int(pen, n)},
-    ##	   nested.lattice = ,
-    ##	    nested.centroids ={
-    ##	      if(
-    ##		 pen[,2] <- pen[,1] + 10
-    ##	  }	      )
-    
-    ## grid.polygon() closes automatically: now '6' where we had '7':
-    n6 <- rep.int(6:6, n)
-    pltx <- rep.int(hexC$x, n) * rep.int(radius, n6) + rep.int(xnew, n6)
-    plty <- rep.int(hexC$y, n) * rep.int(radius, n6) + rep.int(ynew, n6)
-    switch(style,
-           "centroids" = ,
-           "lattice" = {
-             grid.polygon(pltx, plty, default.units=def.unit, id=NULL,
-                          ## density = density,
-                          id.lengths= n6,
-                          gp=gpar(fill = pen, col = border))
-           },
-           "nested.lattice" = ,
-           "nested.centroids" = {
-             grid.polygon(pltx, plty, default.units=def.unit, id=NULL,
-                          id.lengths= n6,
-                          gp=gpar(fill = pen[,2],
-                                  ## density = density,
-                                  col=if(!is.null(border)) border else pen[,2]))
-             
-           })
-    
-  }
+   if (test) {
+     print("hi")
+     mostFreqPen = names(which.max(table(pen)))
+     grid::grid.rect(gp=gpar(fill=mostFreqPen))
+     notMostFreq=(pen!=mostFreqPen)
+     pen = pen[notMostFreq]
+     xnew = xnew[notMostFreq]
+     ynew = ynew[notMostFreq]
+     print(paste(length(xnew),"hexagons drawn out of",length(dat@count),"hexagons"))
+   }
+   hexpolygon(xnew, ynew, hexC,
+              fill = pen)
+   ## and that's been all for these styles
+   return(invisible(paste("done")))
+}
